bc28769af4e1d190338f5ef8758867e94a89f4a6
ashriyaghanekar02-design <ashriyaghanekar02@gmail.com>
2026-06-23 22:00:16 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -4,23 +4,37 @@ const weatherResult = document.getElementById("weatherResult");
 
 async function getWeather(city){
 
-     try{
-    const apiKey = "2e9b25364cd35efcfc786fddd0d61c98";
+    try{
 
-    const url =
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+        const apiKey = "2e9b25364cd35efcfc786fddd0d61c98";
+
+        const url =
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
         const response = await fetch(url);
 
+        if(!response.ok){
+            throw new Error("City not found");
+        }
+
         const data = await response.json();
 
-        console.log(data);
+        weatherResult.innerHTML = `
+        <div class="weather-card">
+            <h2>${data.name}</h2>
+            <p>Temperature: ${data.main.temp} °C</p>
+            <p>Humidity: ${data.main.humidity}%</p>
+            <p>Wind Speed: ${data.wind.speed} m/s</p>
+        </div>
+        `;
 
     }
 
     catch(error){
 
-        console.log(error);
+        weatherResult.innerHTML = `
+        <p>${error.message}</p>
+        `;
 
     }
 }
